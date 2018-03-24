cc6bbcc01131578568d3c5fd498b2d8f200b8fb8
stevensciriha <steven.sciriha@gmail.com>
2018-03-24 16:33:12 +0000

    first commit

---
+++ ansiblePlaybook.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/ksh
+#echo "Please enter the IP or hostname of the desired NTP server:"
+#read  server
+ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook -vvvv -s serverPrep.yml --extra-vars="ntpserver1=$server"
