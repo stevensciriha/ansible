db2992a71f340134caecf2711d6493302a32fe13
stevensciriha <steven.sciriha@gmail.com>
2018-05-08 19:22:46 +0000

    dockerupdate

--- ansiblePlaybook.sh
+++ ansiblePlaybook.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/ksh
 #echo "Please enter the IP or hostname of the desired NTP server:"
 #read  server
-ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook -vvvv -s serverPrep.yml --extra-vars="ntpserver1=$server"
+#ANSIBLE_KEEP_REMOTE_FILES=0 ansible-playbook -vvvv -s serverPrep.yml --extra-vars="ntpserver1=$server"
+ANSIBLE_KEEP_REMOTE_FILES=1 ansible-playbook -vvvv -s serverPrep.yml -u ubuntu
